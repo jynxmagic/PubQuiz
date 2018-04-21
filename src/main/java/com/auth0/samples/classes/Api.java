494867715b795e5ba439e3c0501955833de5b2e8
root <root@vultr.guest>
2018-04-21 10:19:08 +0000

    JSON functionality added to build.gradle

--- src/main/java/com/auth0/samples/classes/Api.java
+++ src/main/java/com/auth0/samples/classes/Api.java
@@ -7,27 +7,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import org.json.JSONObject;
+
 public class Api {	
-	
+
 	public String generateQuestions()
 	{
-        String questions = new String();
-       
-        
+		String questions = new String();
+		JSONObject json = new JSONObject();
+
 		try
 		{
-	        URL questionUrl = new URL("https://opentdb.com/api.php?amount=10");
-	        URLConnection questionConnection = questionUrl.openConnection();
-	        
-	        questionConnection.setDoOutput(true);
-	        Scanner scanner = new Scanner(questionUrl.openStream());
-	        questions = scanner.useDelimiter("\\Z").next();
-	        scanner.close();	        
+			URL questionUrl = new URL("https://opentdb.com/api.php?amount=10");
+			URLConnection questionConnection = questionUrl.openConnection();
+
+			questionConnection.setDoOutput(true);
+			Scanner scanner = new Scanner(questionUrl.openStream());
+			questions = scanner.useDelimiter("\\Z").next();
+			scanner.close();	        
 		}
 		catch(Exception ex)
 		{
-			
+
 		}
-        return questions;
+		return questions;
 	}
 }
